862f21924083ae2e9c365616085ee51050280b43
christinegillis <chrisa8300@gmail.com>
2017-05-03 23:56:41 -0400

    progress

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,24 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
+
+var orm = {
+	selectAll: function(table, cb){
+		var queryString = "SELECT * FROM " + table + ";";
+    connection.query(queryString, function(err, data) {
+      if (err) {
+        throw err;
+      }
+      cb(data);
+    });
+	},
+	insertOne: function(table, ){
+		var queryString = "INSERT into " + table +
+		"(burger_name, devoured, date_) VALUES (?? ?? ??);"; 
+		" "
+
+	},
+	updateOne: function(){}
+}
+
+
+module.exports = orm;
