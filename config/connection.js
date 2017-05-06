d5b84c61c23e7f10a49de755ff6606bdd7635518
christinegillis <chrisa8300@gmail.com>
2017-05-06 01:41:55 -0400

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -8,6 +11,8 @@ var connection = mysql.createConnection({
   password: "chrisa8300@gmail.com",
   database: "burgers_db"
 });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
